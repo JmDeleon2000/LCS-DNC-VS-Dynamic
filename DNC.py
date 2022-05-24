f4e48ba77b54b8f8ae7806a47b9f44de93cc51d2
JmDeleon2000 <54918136+JmDeleon2000@users.noreply.github.com>
2022-05-24 03:41:13 -0600

    Reduced memory cost of DNC

--- DNC.py
+++ DNC.py
@@ -2,19 +2,21 @@ X = 'ABCD'
 Y = 'ACBAD'
 
 def LCS(a, b):
-    if (a == '' or b == ''):
-        return ''
+    def LCS_i(a, b, i, j):
+        if (len(a) == i or len(b) == j):
+            return ''
     
-    if a[0] == b[0]:
-        return a[0] + LCS(a[1::], b[1::]) 
-    x = LCS(a, b[1::])
-    y = LCS(a[1::], b)
+        if a[i] == b[j]:
+            return a[i] + LCS_i(a, b, i+1, j+1) 
+        x = LCS_i(a, b, i, j+1)
+        y = LCS_i(a, b, i+1, j)
 
 
-    if len(x) > len(y):
-        return x
-    return y
+        if len(x) > len(y):
+            return x
+        return y
     
+    return LCS_i(a, b, 0, 0)
 
 print(LCS(X, Y))
-#print(X[1::])
+
