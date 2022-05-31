50d41716520824e7a8104e6baa2d2bd9502d2f0d
srue1834 <53914141+srue1834@users.noreply.github.com>
2022-05-30 18:49:34 -0600

    Cambio en lcs_dp

--- LCS_DP.py
+++ LCS_DP.py
@@ -1,8 +1,10 @@
 import numpy as np
 
-X = 'GTCGTTCGGAATGCCGTTGCTCTGTAAA'
-Y = 'ACCGGTCGAGTGCGCGGAAGCCGGCCGAA'
+# X = 'GTCGTTCGGAATGCCGTTGCTCTGTAAA'
+# Y = 'ACCGGTCGAGTGCGCGGAAGCCGGCCGAA'
 
+Y = 'ABAABA'
+X = 'BABBAB'
 
 X_size = len(X)
 Y_size = len(Y)
@@ -12,19 +14,15 @@ Z = ''
 
 mat = np.zeros((X_size+1, Y_size+1))
 
-for i in range(X_size):
-    for j in range(Y_size):
-        if  X[0] == Y[j]:
-            mat[i+1][j+1] = 1
+for i in range(X_size+1):
+    for j in range(Y_size+1):
+        if i == 0 or j == 0:
+            mat[i][j] = 0
+        elif X[i-1] == Y[j-1]:
+            mat[i][j] = mat[i-1][j-1] + 1
         else:
-            mat[i+1][j+1] = max(mat[i+1][j], mat[i][j+1])
-        if i != 0:
-            if X[i] == Y[j]:
-                mat[i+1][j+1] = mat[i][j]+1
-            else:
-                mat[i+1][j+1] = max(mat[i+1][j], mat[i][j+1])
-
-
+            mat[i][j] = max(mat[i][j-1], mat[i-1][j])
+        
 
 for i in range(X_size):
     if mat[X_size][Y_size] == mat[X_size-1][Y_size]:
